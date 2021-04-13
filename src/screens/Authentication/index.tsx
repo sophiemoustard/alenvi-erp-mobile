663d197d806612695d3f5f57fda4ed5846f8cf94
Manon Palin <manon@compani.fr>
2021-04-13 17:24:30 +0200

    COM-2017 add KeyboardAvoidingView

--- src/screens/Authentication/index.tsx
+++ src/screens/Authentication/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ImageBackground, Text, View, useWindowDimensions } from 'react-native';
+import { ImageBackground, Text, KeyboardAvoidingView, useWindowDimensions, Platform } from 'react-native';
 import NiButton from '../../components/Button';
 import NiInput from '../../components/Input';
 import styles from './styles';
@@ -10,12 +10,12 @@ const Authentication = () => (
     style={{ height: useWindowDimensions().height }}
     source={require('../../../assets/images/authentication_background_image.jpg')}
   >
-    <View style={styles.container}>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.container}>
       <Text style={styles.title}>Identifiez-vous pour accéder aux informations</Text>
       <NiInput style={styles.input} title='Email' />
       <NiInput style={styles.input} title='Mot de Passe' type={PASSWORD} />
       <NiButton title='Se connecter' onPress={() => {}} />
-    </View>
+    </KeyboardAvoidingView>
   </ImageBackground>
 );
 
