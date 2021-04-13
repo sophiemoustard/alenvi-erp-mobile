a3916546823fef0c3beb1bbfbb4f87dc28556e6a
Manon Palin <manon@compani.fr>
2021-04-13 15:49:38 +0200

    COM-2017 add border color on input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput } from 'react-native';
 import styles from './styles';
 
@@ -7,11 +7,17 @@ interface InputProps {
   style: Object,
 }
 
-const Input = ({ title, style }: InputProps) => (
-  <View style={{ ...styles.container, ...style }}>
-    <Text style={styles.caption}>{title}</Text>
-    <TextInput style={styles.input} />
-  </View>
-);
+const Input = ({ title, style }: InputProps) => {
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const inputStyle = styles({ isSelected });
+
+  return (
+    <View style={{ ...inputStyle.container, ...style }}>
+      <Text style={inputStyle.caption}>{title}</Text>
+      <TextInput style={inputStyle.input} onTouchStart={() => setIsSelected(true)}
+        onBlur={() => setIsSelected(false)} />
+    </View>
+  );
+};
 
 export default Input;
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,9 +1,13 @@
 import { StyleSheet } from 'react-native';
-import { WHITE, GREY } from '../../styles/colors';
+import { WHITE, GREY, PINK } from '../../styles/colors';
 import { INPUT_HEIGHT, BORDER_RADIUS, PADDING, MARGIN } from '../../styles/metrics';
 import { FIRA_SANS_REGULAR, FIRA_SANS_MEDIUM } from '../../styles/fonts';
 
-export default StyleSheet.create({
+interface inputStyleProps{
+  isSelected: Boolean,
+}
+
+const inputStyle = ({ isSelected } : inputStyleProps) => StyleSheet.create({
   container: {
     width: '100%',
   },
@@ -11,7 +15,7 @@ export default StyleSheet.create({
     ...FIRA_SANS_MEDIUM.MD,
     height: INPUT_HEIGHT,
     borderRadius: BORDER_RADIUS.MD,
-    borderColor: GREY[600],
+    borderColor: isSelected ? PINK[500] : GREY[600],
     borderWidth: 1,
     paddingHorizontal: PADDING.MD,
     backgroundColor: WHITE,
@@ -23,3 +27,5 @@ export default StyleSheet.create({
     marginBottom: MARGIN.XS,
   },
 });
+
+export default inputStyle;
