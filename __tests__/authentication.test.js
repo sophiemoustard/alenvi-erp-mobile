21b7746306418e9c7e870c1245b0c6ae4f9041ac
Chloe Molle <chloe.molle@neuf.fr>
2021-04-28 15:09:23 +0200

    COM-2026 add test to check if authenticated

--- __tests__/authentication.test.js
+++ __tests__/authentication.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { render, waitFor, cleanup } from '@testing-library/react-native';
+import { render, waitFor, cleanup, fireEvent, act } from '@testing-library/react-native';
 import { Provider as AuthProvider } from '../src/context/AuthContext';
 import AppContainer from '../src/AppContainer';
 import getEnvVars from '../environment';
@@ -35,5 +35,30 @@ describe('authentication', () => {
     );
 
     await waitFor(() => element.getByTestId('authentication'));
+  });
+
+  test('should connect user if right credentials', async () => {
+    axiosMock.onGet(`${baseURL}/version/should-update`, { params: { mobileVersion: '1.0.0', appName: 'erp' } })
+      .reply(200, { data: { mustUpdate: false } })
+      .onPost(`${baseURL}/users/logout`)
+      .reply(200)
+      .onPost(`${baseURL}/users/authenticate`, { email: 'test@alenvi.io', password: '1234567' })
+      .reply(200, { data: { token: 'token', tokenExpireDate: '1234567890', refreshToken: 'refreshToken' } });
+
+    const element = render(
+      <AuthProvider>
+        <AppContainer />
+      </AuthProvider>
+    );
+
+    const emailInput = await waitFor(() => element.queryByTestId('Email'));
+    const passwordInput = await waitFor(() => element.queryByTestId('Mot de Passe'));
+    const sendButton = await waitFor(() => element.getByTestId('Se connecter'));
+
+    await act(async () => fireEvent.changeText(emailInput, 'test@alenvi.io'));
+    await act(async () => fireEvent.changeText(passwordInput, '1234567'));
+    await act(async () => fireEvent.press(sendButton));
+
+    await waitFor(() => element.getByTestId('ProfilePage'));
   });
 });
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -17,7 +17,7 @@ const Profile = () => {
   }, [loggedUser?.picture?.link]);
 
   return (
-    <ScrollView>
+    <ScrollView testID='ProfilePage'>
       <Text style={commonStyle.title}>Mon profil</Text>
       <View style={styles.identityContainer}>
         <View style={styles.profilView}>
