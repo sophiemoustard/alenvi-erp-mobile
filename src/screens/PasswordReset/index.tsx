a8b0631342d4e2b5d4d6e3bed3557e5b9663bf93
Sophie Moustard <sophie.moustard@gmail.com>
2021-05-06 16:09:10 +0200

    COM-2021 - fix naming

--- src/screens/PasswordReset/index.tsx
+++ src/screens/PasswordReset/index.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import PasswordForm from '../../components/PasswordForm';
 import { NavigationType } from '../../types/NavigationType';
 
-interface PasswordEditionProps {
+interface PasswordResetProps {
   route: { params: { userId: string, token: string, email?: string } },
   navigation: NavigationType,
 }
 
-const PasswordEdition = ({ navigation }: PasswordEditionProps) => {
-  const goBack = () => { navigation.navigate('EmailForm'); };
+const PasswordReset = ({ navigation }: PasswordResetProps) => {
+  const goBack = () => { navigation.navigate('ForgotPassword'); };
 
   return (
     <PasswordForm goBack={goBack} />
   );
 };
 
-export default PasswordEdition;
+export default PasswordReset;
