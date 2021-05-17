d72505af9c5f1d77d0cae8ac666ed1de37c8448a
Manon Palin <manon@compani.fr>
2021-05-17 15:37:36 +0200

    COM-2073-add-checkbox extracts renderItem outside radioButtonList

--- src/components/RadioButtonList/index.tsx
+++ src/components/RadioButtonList/index.tsx
@@ -12,27 +12,27 @@ interface RadioButtonProps {
 
 const renderSeparator = () => <View style={styles.separator} />;
 
+const renderItem = (item: any, checkedRadioButton: Number | null, onPressCheckbox: (value: Number) => void) => {
+  const iconName = checkedRadioButton === item.value ? 'radio-button-checked' : 'radio-button-unchecked';
+  const iconColor = checkedRadioButton === item.value ? BLACK : GREY[600];
+  const textStyle = checkedRadioButton === item.value ? styles.text : { ...styles.text, color: GREY[600] };
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={() => onPressCheckbox(item.value)}>
+      <MaterialIcons style={styles.icon} size={20} name={iconName} color={iconColor} />
+      <Text style={textStyle}>{item.label}</Text>
+    </TouchableOpacity>
+  );
+};
+
 const RadioButtonList = ({ options }: RadioButtonProps) => {
   const [checkedRadioButton, setCheckedRadioButton] = useState<Number | null>(null);
 
-  const onPressCheckbox = (value: Number) =>
-    setCheckedRadioButton(prevValue => (prevValue === value ? null : value));
+  const onPressCheckbox = (value: Number) => setCheckedRadioButton(prevValue => (prevValue === value ? null : value));
 
-  const renderItem = (item: any) => {
-    const iconName = checkedRadioButton === item.value ? 'radio-button-checked' : 'radio-button-unchecked';
-    const iconColor = checkedRadioButton === item.value ? BLACK : GREY[600];
-    const textStyle = checkedRadioButton === item.value ? styles.text : { ...styles.text, color: GREY[600] };
-
-    return (
-      <TouchableOpacity style={styles.container} onPress={() => onPressCheckbox(item.value)}>
-        <MaterialIcons style={styles.icon} size={20} name={iconName} color={iconColor} />
-        <Text style={textStyle}>{item.label}</Text>
-      </TouchableOpacity>
-    );
-  };
   return (
-    <FlatList data={options} keyExtractor={item => item.label}
-      renderItem={({ item }) => renderItem(item)} ItemSeparatorComponent={renderSeparator} />
+    <FlatList data={options} keyExtractor={item => item.label} ItemSeparatorComponent={renderSeparator}
+      renderItem={({ item }) => renderItem(item, checkedRadioButton, onPressCheckbox)} />
   );
 };
 
