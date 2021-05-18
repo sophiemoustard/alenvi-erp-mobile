64268b443e78db01f699bd05cbaf7574c00b24cf
Manon Palin <manon@compani.fr>
2021-05-18 11:02:56 +0200

    COM-2046 create EditProfil screen & navigation

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -17,6 +17,8 @@ const Profile = () => {
     navigation.navigate('PasswordEdition', { userId: loggedUser?._id })
   );
 
+  const goToEditProfil = () => (navigation.navigate('ProfileEdition'));
+
   useEffect(() => {
     if (loggedUser?.picture?.link) setSource({ uri: loggedUser.picture.link });
     else setSource(require('../../../assets/images/default_avatar.png'));
@@ -43,7 +45,7 @@ const Profile = () => {
         <Text style={styles.infos}>{loggedUser?.local?.email}</Text>
       </View>
       <View style={styles.buttonContainer}>
-        <NiSecondaryButton title='Modifier mes informations' onPress={() => {}} style={styles.button} />
+        <NiSecondaryButton title='Modifier mes informations' onPress={goToEditProfil} style={styles.button} />
         <NiSecondaryButton title='Modifier mon mot de passe' onPress={goToPasswordReset} />
       </View>
       <View style={styles.sectionDelimiter} />
---
+++ src/screens/ProfileEdition/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const ProfileEdition = () => (
+  <Text>Profil Edition</Text>
+);
+
+export default ProfileEdition;
