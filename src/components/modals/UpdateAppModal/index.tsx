b4b21345c5c170733c5d74a70a93a79418e1e40f
Sophie Moustard <sophie.moustard@gmail.com>
2021-05-20 14:19:57 +0200

    COM-2025 - add store redirections

--- src/components/modals/UpdateAppModal/index.tsx
+++ src/components/modals/UpdateAppModal/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Linking, Platform } from 'react-native';
 import NiModal from '../Modal';
 import NiPrimaryButton from '../../form/PrimaryButton';
 import styles from './styles';
@@ -9,16 +9,22 @@ interface UpdateAppModalProps {
   visible: boolean,
 }
 
-const UpdateAppModal = ({ visible }: UpdateAppModalProps) => (
-  <NiModal visible={visible}>
-    <>
-      <Text style={modalStyles.title}> Nouvelle version de l&apos;app disponible !</Text>
-      <Text style={[modalStyles.body, styles.body]}>
-        Merci de mettre à jour votre application pour pouvoir continuer à l&apos;utiliser :)
-      </Text>
-      <NiPrimaryButton title="Mettre à jour" onPress={() => {}}/>
-    </>
-  </NiModal>
-);
+const UpdateAppModal = ({ visible }: UpdateAppModalProps) => {
+  const appUrl = Platform.OS === 'ios'
+    ? 'https://apps.apple.com/app/id/1564254334'
+    : 'market://details?id=com.compani.erp';
+
+  return (
+    <NiModal visible={visible}>
+      <>
+        <Text style={modalStyles.title}> Nouvelle version de l&apos;app disponible !</Text>
+        <Text style={[modalStyles.body, styles.body]}>
+          Merci de mettre à jour votre application pour pouvoir continuer à l&apos;utiliser :)
+        </Text>
+        <NiPrimaryButton title="Mettre à jour" onPress={() => { Linking.openURL(appUrl); }}/>
+      </>
+    </NiModal>
+  );
+};
 
 export default UpdateAppModal;
