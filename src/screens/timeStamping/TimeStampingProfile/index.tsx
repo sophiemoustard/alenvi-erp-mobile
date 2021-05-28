f9ae174798ac99f270907ec2406fa72e29fc018f
Manon Palin <manon@compani.fr>
2021-05-28 16:11:11 +0200

    COM-2103 add useFocusEffect to refresh events when timeStampingScreen is focused

--- src/screens/timeStamping/TimeStampingProfile/index.tsx
+++ src/screens/timeStamping/TimeStampingProfile/index.tsx
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect, useState, useCallback } from 'react';
+import React, { useContext, useEffect, useState, useCallback, useRef } from 'react';
 import { Text, View, ScrollView } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import commonStyle from '../../../styles/common';
 import Events from '../../../api/Events';
 import { Context as AuthContext } from '../../../context/AuthContext';
@@ -18,44 +19,50 @@ const renderEvent = (event: EventType) => (
 );
 
 const TimeStampingProfile = () => {
-  const [currentDate, setCurrentDate] = useState<Date>(new Date());
+  const [displayedDate, setdisplayedDate] = useState<Date>(new Date());
   const [events, setEvents] = useState<EventType[]>([]);
-
+  const isActive = useRef<boolean>(true);
   const { loggedUser } = useContext(AuthContext);
 
   useEffect(() => {
-    const interval = setInterval(() => { setCurrentDate(new Date()); }, 60000);
+    const interval = setInterval(() => { setdisplayedDate(new Date()); }, 60000);
     return () => clearInterval(interval);
   }, []);
 
-  const fetchInterventions = useCallback(async () => {
-    try {
-      if (!loggedUser || !loggedUser._id) return;
-      const params = {
-        auxiliary: loggedUser._id,
-        startDate: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 0, 0, 0, 0),
-        endDate: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 23, 59, 59, 999),
-        type: INTERVENTION,
+  useFocusEffect(
+    useCallback(() => {
+      isActive.current = true;
+      const fetchInterventions = async () => {
+        const today = new Date();
+        try {
+          if (!loggedUser || !loggedUser._id) return;
+          const params = {
+            auxiliary: loggedUser._id,
+            startDate: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0, 0),
+            endDate: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59, 999),
+            type: INTERVENTION,
+          };
+          const fetchedEvents = await Events.events(params);
+
+          if (isActive) setEvents(fetchedEvents);
+        } catch (e) {
+          console.error(e);
+        }
       };
-      const fetchedEvents = await Events.events(params);
-
-      setEvents(fetchedEvents);
-    } catch (e) {
-      console.error(e);
-    }
-  }, [loggedUser, currentDate]);
-
-  useEffect(() => {
-    if (loggedUser?._id) fetchInterventions();
-  }, [loggedUser, fetchInterventions]);
+      fetchInterventions();
+      return () => {
+        isActive.current = false;
+      };
+    }, [loggedUser, isActive])
+  );
 
   return (
     <ScrollView style={styles.screen} testID="TimeStampingProfile">
       <Text style={commonStyle.title}>Horodatage</Text>
       <View style={styles.container}>
         <View>
-          <Text style={styles.date}>{capitalizeFirstLetter(formatDate(currentDate))}</Text>
-          <Text style={styles.time}>{formatTime(currentDate)}</Text>
+          <Text style={styles.date}>{capitalizeFirstLetter(formatDate(displayedDate))}</Text>
+          <Text style={styles.time}>{formatTime(displayedDate)}</Text>
         </View>
         <View style={styles.viewIntervention}>
           <Text style={styles.textIntervention}>{events.length} {formatWordToPlural(events, 'intervention')}</Text>
