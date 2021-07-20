6c6a4c5233795974a7a8d499d92eeb4e09d13028
KennyCallegari <mel.kenny@gmail.com>
2021-07-20 14:22:54 +0200

    COM-2189 - use reducer

--- src/screens/timeStamping/QRCodeScanner/index.tsx
+++ src/screens/timeStamping/QRCodeScanner/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useReducer } from 'react';
 import { View, Alert, TouchableOpacity, Text, ActivityIndicator, Image } from 'react-native';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { Camera } from 'expo-camera';
@@ -25,12 +25,41 @@ interface QRCodeScannerProps {
   }
 }
 
+interface StateType {
+  errorMessage: string,
+  scanned: boolean,
+  loading: boolean,
+}
+interface ActionType {
+  type: string,
+  payload?: { errorMessage: string },
+}
+
+const SCANNING = 'scanning';
+const WRONG_QR_CODE = 'wrong_qr_code';
+const BAD_REQUEST = 'bad_request';
+
+const reducer = (state: StateType, action: ActionType): StateType => {
+  switch (action.type) {
+    case SCANNING:
+      return { loading: true, scanned: true, errorMessage: '' };
+    case WRONG_QR_CODE:
+      return {
+        loading: false,
+        scanned: false,
+        errorMessage: 'Le QR code scanné ne correspond pas au bénéficiaire de l\'intervention',
+      };
+    case BAD_REQUEST:
+      return { loading: false, scanned: true, errorMessage: action.payload?.errorMessage || '' };
+    default:
+      return state;
+  }
+};
+
 const QRCodeScanner = ({ route }: QRCodeScannerProps) => {
+  const [state, dispatch] = useReducer(reducer, { errorMessage: '', scanned: false, loading: false });
   const [hasPermission, setHasPermission] = useState<boolean>(false);
   const [modalVisible, setModalVisible] = useState<boolean>(false);
-  const [scanned, setScanned] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>('');
 
   const navigation = useNavigation();
 
@@ -58,14 +87,10 @@ const QRCodeScanner = ({ route }: QRCodeScannerProps) => {
   );
 
   const handleBarCodeScanned = async ({ data }: BarCodeType) => {
-    setScanned(true);
-    setLoading(true);
-    setErrorMessage('');
+    dispatch({ type: SCANNING });
     try {
       if (data !== route.params.event.customer._id) {
-        setErrorMessage('Le QR code scanné ne correspond pas au bénéficiaire de l\'intervention');
-        setScanned(false);
-        setLoading(false);
+        dispatch({ type: WRONG_QR_CODE });
         return;
       }
 
@@ -74,11 +99,16 @@ const QRCodeScanner = ({ route }: QRCodeScannerProps) => {
 
       goBack();
     } catch (e) {
-      if ([409, 422].includes(e.response.status)) setErrorMessage(e.response.data.message);
-      else if ([404, 403].includes(e.response.status)) setErrorMessage('Vous ne pouvez pas horodater cet évènement.');
-      else setErrorMessage('Erreur, si le problème persiste, contactez le support technique.');
-      setScanned(true);
-      setLoading(false);
+      if ([409, 422].includes(e.response.status)) {
+        dispatch({ type: BAD_REQUEST, payload: { errorMessage: e.response.data.message } });
+      } else if ([404, 403].includes(e.response.status)) {
+        dispatch({ type: BAD_REQUEST, payload: { errorMessage: 'Vous ne pouvez pas horodater cet évènement.' } });
+      } else {
+        dispatch({
+          type: BAD_REQUEST,
+          payload: { errorMessage: 'Erreur, si le problème persiste, contactez le support technique.' },
+        });
+      }
     }
   };
 
@@ -105,26 +135,27 @@ const QRCodeScanner = ({ route }: QRCodeScannerProps) => {
   };
 
   return (
-    <Camera onBarCodeScanned={!hasPermission || scanned || loading ? undefined : handleBarCodeScanned}
-      style={styles.container} barCodeScannerSettings={{ barCodeTypes: ['org.iso.QRCode'] }}>
-      <View style={styles.backgroundContainer}>
-        <Image source={{ uri: 'https://storage.googleapis.com/compani-main/qr-code-background.png' }}
-          style={styles.background}/>
-      </View>
+    <Camera onBarCodeScanned={!hasPermission || state.scanned || state.loading ? undefined : handleBarCodeScanned}
+      style={styles.container} barCodeScannerSettings={{ barCodeTypes: ['org.iso.QRCode'] }}
+      type={Camera.Constants.Type.back}>
       <View>
         <FeatherButton name='x-circle' onPress={goBack} size={ICON.LG} color={WHITE} style={styles.closeButton} />
         <Text style={styles.title}>{'Début de l\'intervention'}</Text>
         <EventInfoCell identity={route.params.event.customer.identity} style={styles.cell} />
       </View>
       <View>
-        {loading && <ActivityIndicator color={WHITE} size="small" />}
-        {!!errorMessage && <NiErrorCell message={errorMessage} />}
+        {state.loading && <ActivityIndicator color={WHITE} size="small" />}
+        {!!state.errorMessage && <NiErrorCell message={state.errorMessage} />}
         <TouchableOpacity onPress={() => goToManualTimeStamping(true)}>
           <Text style={styles.manualTimeStampingButton}>Je ne peux pas scanner le QR code</Text>
         </TouchableOpacity>
       </View>
       <CameraAccessModal visible={modalVisible} onPressDismiss={() => setModalVisible(false)}
         onPressAskAgain={askPermissionAgain} />
+      <View style={styles.backgroundContainer}>
+        <Image source={{ uri: 'https://storage.googleapis.com/compani-main/qr-code-background.png' }}
+          style={styles.background}/>
+      </View>
     </Camera>
   );
 };
