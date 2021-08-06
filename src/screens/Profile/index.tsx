b019cd8114a7c01602ed99fcc3b78bfba1efbf53
CorentinMartinot <corentin.martinot@gmail.com>
2021-08-06 15:46:23 +0200

    test-github-actions some changes

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -26,7 +26,7 @@ const Profile = () => {
 
   return (
     <ScrollView contentContainerStyle={styles.screen}>
-      <Text style={commonStyle.title}>Mon profil</Text>
+      <Text style={commonStyle.title}>Mon profil test</Text>
       <View style={styles.identityContainer}>
         <View style={styles.profilView}>
           <Image source={source} style={styles.image} />
