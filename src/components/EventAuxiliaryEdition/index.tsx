15e194e791f39f1913e1b48c5cfca75c86fb5816
Sophie Moustard <sophiemoustard@sophies-macbook-pro.home>
2021-12-03 16:24:51 +0100

    COM-2523 - update design on long name

--- src/components/EventAuxiliaryEdition/index.tsx
+++ src/components/EventAuxiliaryEdition/index.tsx
@@ -46,8 +46,8 @@ const EventAuxiliaryEdition = ({
         <TouchableOpacity style={styles.auxiliaryInfos} onPress={onPress}>
           <Image source={auxiliaryPicture} style={styles.image} />
           <Text style={styles.auxiliaryText}>{formatIdentity(auxiliary.identity, 'FL')}</Text>
+          {isEditable && <FeatherButton name='chevron-down' onPress={() => setAuxiliaryEditionModal(true)} />}
         </TouchableOpacity>
-        {isEditable && <FeatherButton name='chevron-down' onPress={() => setAuxiliaryEditionModal(true)} />}
         <EventAuxiliaryEditionModal visible={auxiliaryEditionModal} auxiliaryOptions={auxiliaryOptions}
           onRequestClose={() => setAuxiliaryEditionModal(false)} eventEditionDispatch={eventEditionDispatch} />
       </View>
