c257f0556b7da182caf53dc1dd4a7820fd700553
Manon Palin <manon@compani.fr>
2021-12-23 10:50:55 +0100

    COM-2582 - boyscoot

--- src/api/Events.ts
+++ src/api/Events.ts
@@ -4,22 +4,23 @@ import { EventTypeEnum } from '../types/EventType';
 
 export type timeStampEventPayloadType = { action: string, reason?: string, startDate?: Date, endDate?: Date };
 type updateEventsPayloadType = { auxiliary: string, startDate: Date, endDate: Date, kmDuringEvent: number };
+type getEventsQueryType = {
+  auxiliary: string,
+  startDate: string,
+  endDate: string,
+  type: EventTypeEnum,
+  isCancelled: boolean
+};
 
 export default {
-  list: async (params: {
-    auxiliary: string,
-    startDate: Date,
-    endDate: Date,
-    type: EventTypeEnum,
-    isCancelled: boolean
-  }) => {
+  list: async (params: getEventsQueryType) => {
     const baseURL = await Environment.getBaseUrl();
     const events = await axiosLogged.get(`${baseURL}/events`, { params });
     return events.data.data.events;
   },
-  timeStampEvent: async (id: string, data: timeStampEventPayloadType) => {
+  timeStampEvent: async (id: string, payload: timeStampEventPayloadType) => {
     const baseURL = await Environment.getBaseUrl();
-    await axiosLogged.put(`${baseURL}/events/${id}/timestamping`, data);
+    await axiosLogged.put(`${baseURL}/events/${id}/timestamping`, payload);
   },
   updateById: async (eventId: string, payload: updateEventsPayloadType) => {
     const baseURL = await Environment.getBaseUrl();
