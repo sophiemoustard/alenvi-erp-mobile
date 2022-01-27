90e6cd8ead4160133d4778287661754ba3b82cf1
Manon Palin <manon@compani.fr>
2022-01-27 16:24:45 +0100

    COM-2647 - create toggle animation

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from 'react';
-import { TouchableWithoutFeedback, View, Animated } from 'react-native';
+import React, { useRef, useEffect, useState } from 'react';
+import { TouchableWithoutFeedback, View, Animated, LayoutChangeEvent } from 'react-native';
 import { COPPER_GREY, WHITE } from '../../styles/colors';
+import { MARGIN } from '../../styles/metrics';
 import styles from './styles';
 
 type OptionType = { label: string, value: boolean };
@@ -17,6 +18,10 @@ const Switch = ({ value, options, backgroundColor, unselectedTextColor, onChange
   const switchStyles = styles({ backgroundColor, unselectedTextColor });
   const animationTextLeftValue = useRef(new Animated.Value(value ? 1 : 0)).current;
   const animationTextRightValue = useRef(new Animated.Value(value ? 0 : 1)).current;
+  const animationToggleValue = useRef(new Animated.Value(value ? 1 : 0)).current;
+  const [containerWidth, setContainerWidth] = useState<number>(0);
+
+  const onLayout = ({ nativeEvent: { layout: { width } } }: LayoutChangeEvent) => { setContainerWidth(width); };
 
   useEffect(() => {
     Animated.timing(
@@ -28,9 +33,20 @@ const Switch = ({ value, options, backgroundColor, unselectedTextColor, onChange
       animationTextRightValue,
       { toValue: value ? 0 : 1, duration: 300, useNativeDriver: false }
     ).start();
-  }, [animationTextLeftValue, animationTextRightValue, value]);
+
+    Animated.timing(
+      animationToggleValue,
+      { toValue: value ? 0 : 1, duration: 300, useNativeDriver: true }
+    ).start();
+  }, [animationTextLeftValue, animationTextRightValue, animationToggleValue, value]);
 
   const dynamicStyles = {
+    toggle: (animation: Animated.Value) => ({
+      transform: [{ translateX: animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, containerWidth - MARGIN.XS * 2],
+      }) }],
+    }),
     text: (animation: Animated.Value) => ({
       color: animation.interpolate({ inputRange: [0, 1], outputRange: [WHITE, COPPER_GREY[900]] }),
     }),
@@ -38,12 +54,15 @@ const Switch = ({ value, options, backgroundColor, unselectedTextColor, onChange
 
   return <TouchableWithoutFeedback onPress={onChange}>
     <View style={switchStyles.container}>
-      <Animated.Text style={[dynamicStyles.text(animationTextLeftValue), switchStyles.text]}>
-        {options[0]?.label}
-      </Animated.Text>
-      <Animated.Text style={[dynamicStyles.text(animationTextRightValue), switchStyles.text]}>
-        {options[1]?.label}
-      </Animated.Text>
+      <Animated.View style={[dynamicStyles.toggle(animationToggleValue), switchStyles.toggle]} onLayout={onLayout} />
+      <View style={switchStyles.textContainer}>
+        <Animated.Text style={[dynamicStyles.text(animationTextLeftValue), switchStyles.text]}>
+          {options[0]?.label}
+        </Animated.Text>
+        <Animated.Text style={[dynamicStyles.text(animationTextRightValue), switchStyles.text]}>
+          {options[1]?.label}
+        </Animated.Text>
+      </View>
     </View>
   </TouchableWithoutFeedback>;
 };
--- src/components/Switch/styles.ts
+++ src/components/Switch/styles.ts
@@ -1,5 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { BORDER_RADIUS, PADDING, MARGIN, BUTTON_HEIGHT } from '../../styles/metrics';
+import { WHITE } from '../../styles/colors';
 import { FIRA_SANS_MEDIUM } from '../../styles/fonts';
 
 type SwitchStyleProps = {
@@ -11,16 +12,28 @@ const switchStyle = ({ backgroundColor, unselectedTextColor } : SwitchStyleProps
   container: {
     backgroundColor,
     borderRadius: BORDER_RADIUS.XXL,
-    paddingHorizontal: PADDING.LG,
     height: BUTTON_HEIGHT,
     marginHorizontal: MARGIN.LG,
     marginBottom: MARGIN.XL,
+    justifyContent: 'center',
+  },
+  textContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: PADDING.LG,
     justifyContent: 'space-around',
+    height: '100%',
   },
   text: {
     ...FIRA_SANS_MEDIUM.MD,
+  },
+  toggle: {
+    backgroundColor: WHITE,
+    width: '50%',
+    height: BUTTON_HEIGHT - PADDING.SM * 2,
+    position: 'absolute',
+    marginHorizontal: MARGIN.XS,
+    borderRadius: BORDER_RADIUS.XL,
   },
 });
 
